c726d53c12b73aa0df029a3651a44bf285428e07
Yara <yara@example.com>
2026-08-11 17:37:22 +0000

    update training management module final changes

--- custom/addons/training_management/__manifest__.py
+++ custom/addons/training_management/__manifest__.py
@@ -1,19 +1,18 @@
 {
     'name': 'Training Management',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     'category': 'Education',
     'summary': 'Manage training and courses',
     'description': 'A module for managing summer training applications and courses.',
     'depends': ['base', 'mail'],
  'data': [
+        'security/training_security.xml',
         'security/ir.model.access.csv',
         'data/ir_sequence_data.xml',
         'views/summer_training_views.xml',
         'views/summer_training_trainee_views.xml',
         'views/summer_training_team_views.xml',
-        'views/summer_training_survey_views.xml'
-
-
+        'views/summer_training_survey_views.xml',
     ],
     'installable': True,
     'application': True,
